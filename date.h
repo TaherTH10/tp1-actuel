1826c119c6f7399622cdfb4b3723a6f82c86fd16
TaherTH10 <tarikgbazzi@gmail.com>
2025-01-06 10:35:01 +0100

    hello

---
+++ date.h
@@ -0,0 +1,20 @@
+class Date {
+public:
+ Date(int month=1, int day=1,int annee=1);
+ int month() const;
+ int day() const;
+ int annee() const;
+ void updateMonth(int month);
+ void updateDay(int day);
+ void updateAnnee(int annee);
+ void next();
+ void back();
+private:
+ int _month;
+ int _day;
+ int _annee;
+};
+bool isDate(int month, int day,int annee);
+int getDaysInMonth(int month);
+int dayOfYear(Date d);
+std::string toString(Date d);
---
+++ livre.h
@@ -0,0 +1,27 @@
+class livre
+{
+private:
+    std::string _titre;
+    std::string _auteur;
+    std::string _langue;
+    std::string _genre;
+    std::string _datepu;
+    int _ISBN;
+public:
+    livre(std::string t,std::string a,std::string l,std::string g,std::string,int ISBN );
+    std::string titre() const;
+    std::string auteur() const;
+    std::string langue() const;
+    std::string genre() const;
+    std::string datep() const;
+    int ISBN() const;
+    void settitre(std::string titre);
+    void setauteur(std::string auteur);
+    void setlangue(std::string langue);
+    void setgenre( std::string genre);
+    void setdatep(std::string datep);
+    int setISBN(int ISBN);
+
+};
+
+
